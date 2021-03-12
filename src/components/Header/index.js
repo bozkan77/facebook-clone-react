973b215c696a30a793bb71c48aa6b79896e66bbc
Burak Özkan <burak.ozkan@dokabilisim.com>
2021-03-12 12:32:57 +0300

    add react app and material ui

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>test('should ', () => {
+        
+      })
+      </h2>
+      <div className="header__left">
+        <image src="https://www.penege.com/wp-content/uploads/2017/02/facebook-logo-02.png" />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjo1DkndpOykkQkTbZDK1Y6GWG1IoBFb8",
+  authDomain: "facebook-clone-27f4e.firebaseapp.com",
+  projectId: "facebook-clone-27f4e",
+  storageBucket: "facebook-clone-27f4e.appspot.com",
+  messagingSenderId: "1021169596341",
+  appId: "1:1021169596341:web:726683a434411223f40bfc"
+};
